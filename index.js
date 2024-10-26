0c94cb3cb83469e856d33d2463ca5c2401fe58b0
manancode <mananarorawork@gmail.com>
2024-10-27 03:36:06 +0530

    register-challenge

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { randomUUID } = require("crypto")
 const express = require("express")
 const {generateRegistrationOptions} = require("@simplewebauthn/server")
-const { error } = require("console")
+const cors = require("cors")
+const crypto = require("node:crypto")
+
+if(!globalThis.crypto){
+    globalThis.crypto = crypto
+}
 
 const app = express()
-
+app.use(cors())
 app.use(express.static('./public'))
 app.use(express.json())
 
@@ -27,6 +32,7 @@ app.post("/signup" , (req,res)=>{
 
 app.post("/register-challenge" , async (req,res)=>{
     const {userid} = req.body
+    const user = userStore[userid];
     if (!userStore[userid]) return res.status(404).json({error : "user not found"})
     const registrationOptions = await generateRegistrationOptions({
     rpName : "example.com",
@@ -42,6 +48,8 @@ app.post("/register-challenge" , async (req,res)=>{
 
 })
 
+
+app.post("/register-verify" , async )
 
 app.listen(3000 , ()=>{
     console.log("server is running on port 3000")
